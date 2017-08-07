5b4bd9b5c195c763547b622f92bc01a5ed02de5c
SkySCC <30620767+SkySCC@users.noreply.github.com>
2017-08-07 16:49:01 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 
 //change the text of the main-Text div
-var element = document.getElementById(main-Text);
+var element = document.getElementById('main-Text');
 
 element.innerHTML = 'new Value';
 
