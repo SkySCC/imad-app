002d56cd9fb8d9f59354e332fe535b4032145336
SkySCC <30620767+SkySCC@users.noreply.github.com>
2017-08-07 17:21:04 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element = document.getElementById('main-Text');
 element.innerHTML = 'new Value';
 
 //Move the image
-var img = document.getElementById('madi.png');
-img.onclick = function () {
+var img = document.getElementById('madi');
+img.onclick = function (madi) {
 img.style.marginLeft = '200px';  
 };
