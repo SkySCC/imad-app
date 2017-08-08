05819e746ee0c0351aea69110af1727d064bc213
SkySCC <30620767+SkySCC@users.noreply.github.com>
2017-08-08 12:32:33 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,27 @@
 //Counter code
 var button = document.getElementById('Counter');
-var counter = 0;
+
 button.onclick = function () {
   
-  //Make a request to the counter endpoint
+  //Create a request object
+  var request = new XMLHTTPRequest();
+  
   
   //Capture the response and store it in a variable
+  request.onreadystatechange = function () {
+    if (request.readyState === XMLHTTPRequest.DONE) {  
+     //Take some action
+     if (request.status === 200) {
+         var counter =  request.responseText;
+         var span = document.getElementById('count');
+         span.innerHTML = counter.toString();
+        }
+    }
+     //Not done yet
+  };
+  //Make the request
+  request.open('GET', 'http://minersgroupmcpe.imad.hasura-app.io/counter', true);
+  request.send(null);
   
-  //Render the variable in the corredct span
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+  
 };
