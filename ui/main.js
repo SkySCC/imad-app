deeea17ac4fa9f96c29fc49f373b6754ea577e55
SkySCC <30620767+SkySCC@users.noreply.github.com>
2017-08-08 12:08:05 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ button.onclick = function () {
   //Capture the response and store it in a variable
   
   //Render the variable in the corredct span
-  Counter = Counter + 1;
+  counter = counter + 1;
   var span = document.getElementById('count');
-  span.innerHTML = counter.toString ();
+  span.innerHTML = counter.toString();
 };
