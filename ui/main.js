d355a55fbbcab73440e6e142fa4b4aaab46fb324
SkySCC <30620767+SkySCC@users.noreply.github.com>
2017-08-08 17:15:50 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,8 +4,10 @@ var button = document.getElementById('counter');
 button.onclick = function () {
   
   //Create a request object
-  var request = new XMLHTTPRequest();
+  var request = new XMLHTTPRequest ();
+  
   //Capture the response and store it in a variable
+  
   request.onreadystatechange = function () {
     if (request.readyState === XMLHTTPRequest.DONE) {  
      //Take some action
