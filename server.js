b24c5a9e3cf8d93f0d9a327a8fdfb2f5bfe7eefb
SkySCC <30620767+SkySCC@users.noreply.github.com>
2017-08-09 12:01:48 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,17 @@ app.get('/counter', function (req, res) {
    res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name', function (req, res) { //URL: /submit-name?name=xxxxxx
+   //Get the name from the request
+   var name = req.query.name;
+   
+   names.push(name);
+   
+  //JSON: JavaScript Object Notation
+   res.send(JSON.stringify(names));
+});
+
 app.get('/;articleName', function (req, res) {
     //articleName == Article-One
     //Artcles[articleName] == {} content object for Article One
@@ -106,18 +117,6 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
-var names = [];
-app.get('/submit-name', function (req, res) { //URL: /submit-name?name=xxxxxx
-   //Get the name from the request
-   var name = req.query.name;
-   
-   names.push(name);
-   
-  //JSON: JavaScript Object Notation
-   res.send(JSON.stringify(names));
-});
-
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
