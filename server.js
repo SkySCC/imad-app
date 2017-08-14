1642a225b7d2b7a58a8a996f8c365fae59c63e6c
SkySCC <30620767+SkySCC@users.noreply.github.com>
2017-08-14 12:27:56 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,7 +125,20 @@ app.get('/;articleName', function (req, res) {
     //articleName == Article-One
     //Artcles[articleName] == {} content object for Article One
     var articleName = req.params.articleName ;
-   res.send (createTemplate(Articles[articleName]));
+
+    
+   pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function (err, result){ 
+       if (err) {
+           res.status(500).send(err.toString());
+       } else {
+           if (result.rows.length === 0) {
+           res.status(404).send('Article not found');
+            } else {
+                var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+            }
+       }
+   });
 });
 
 app.get('/ui/style.css', function (req, res) {
